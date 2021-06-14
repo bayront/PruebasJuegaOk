d472e9031605856ca6a7e9cad45d326559174330
Bayron Toruño <bayron.toruno@intra.juegaok.com>
2021-06-14 10:45:35 -0600

    Agregar archivos de proyecto.

---
+++ JuegaOk2/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace JuegaOk2
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.min.js",                        
+                        "~/Scripts/moment.min.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/DataTables/jquery.dataTables.min.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap.min.js",
+                        "~/Scripts/bootstrap-datetimepicker.min.js",
+                        "~/Scripts/defauldSystem.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.min.css",
+                      "~/Content/bootstrap-datetimepicker.min.css",
+                      "~/Content/sweetalert/sweet-alert.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ JuegaOk2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JuegaOk2.MvcApplication" Language="C#" %>
